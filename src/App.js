728bd35a8a91b5a4c1f78da28f7ac05045e426b6
William Dias <smdwilliam89@gmail.com>
2022-06-20 16:09:23 +0100

    Migrado o App component para Class Component e implementado o state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import React from "react";
 import "./App.css";
 import Comment from "./components/Comment";
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>My Comments App</h1>
-      <Comment
-        name='William Dias'
-        email='smdwilliam89@gmail.com'
-        date={new Date()}
-        message='I am learning React with Jonathan.'
-      />
+class App extends React.Component {
+  state = {
+    comments: [
+      {
+        name: "William Dias",
+        email: "smdwilliam89@gmail.com",
+        date: new Date(),
+        message: "I am learning React with Jonathan",
+      },
+      {
+        name: "Bárbara Moreira",
+        email: "bcarvalho089@gmail.com",
+        date: new Date(),
+        message: "My husband is learning React with Jonathan.",
+      },
+    ],
+  };
 
-      <Comment
-        name='Bárbara Moreira'
-        email='bcarvalho089@gmail.com'
-        date={new Date()}
-        message='I am learning React with Jonathan.'
-      />
-    </div>
-  );
+  addComment = () => {
+    const newComment = {
+      name: "Felipe Melo",
+      email: "felipe@gmail.com",
+      date: new Date(),
+      message: "Exemplo de comentário",
+    };
+
+    this.setState({
+      comments: [...this.state.comments, newComment],
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>My Comments App</h1>
+        {this.state.comments.map((comment, index) => {
+          return (
+            <Comment
+              key={index}
+              name={comment.name}
+              email={comment.email}
+              date={comment.date}
+              message={comment.message}
+            />
+          );
+        })}
+        <button onClick={this.addComment}>Add comment</button>
+      </div>
+    );
+  }
 }
 
 export default App;
