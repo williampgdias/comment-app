5ae7b1102e3ec0e3ee3f399e285d914a43acada8
William Dias <smdwilliam89@gmail.com>
2022-06-20 15:16:43 +0100

    Project created and comment created

---
+++ src/components/Comment.jsx
@@ -0,0 +1,12 @@
+import "./Comment.style.css";
+
+const Comment = () => {
+  return (
+    <div className='comment'>
+      <h2>William Dias</h2>
+      <p>This is the Comment Component.</p>
+    </div>
+  );
+};
+
+export default Comment;
